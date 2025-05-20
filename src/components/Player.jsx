c676217085726709b8bf915eb7dbab5d966b594a
Diana Semenova <semenovadiana260@gmail.com>
2025-05-20 09:45:27 +0100

    Initial commit of React project

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Player({initialName, symbol, isActive}) {
+
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    let editablePlayerName  = <span className="player-name">{playerName}</span>;
+    let btnCaption = 'Edit';
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    };
+
+    function handleEditClick (){
+        setIsEditing((editing) => !editing);
+    }
+
+    if(isEditing) {
+        editablePlayerName = <span className="player">
+            <input 
+            type="text" required 
+            value={playerName} 
+            onChange={handleChange}/></span>;
+        btnCaption = 'Save';
+    }
+
+    return (
+        <li className={isActive ? 'active' : undefined }>
+            <span className="players">
+                {editablePlayerName}
+            <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{btnCaption}</button>
+          </li>
+    );
+}
